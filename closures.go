286d6e2d3ca0c4536181d25f223ecc8919b93da6
Rashmi Nagpal <rashminagpal.nagpal@gmail.com>
2019-04-14 09:50:50 +0530

    Add files via upload

---
+++ closures.go
@@ -0,0 +1,22 @@
+// Anonymous functions are useful when you want to define a function inline without having to name it.
+package main
+import "fmt"
+
+// This function `intSeq` returns another function, which we define anonymously in the body of `intSeq`. The
+// returned function _closes over_ the variable `i` to form a closure.
+func intSeq() func() int {
+    i := 0
+    return func() int {
+        i++
+        return i }
+}
+func main() {
+    // We call `intSeq`, assigning the result (a function)
+    // to `nextInt`. This function value captures its
+    // own `i` value.
+    nextInt := intSeq()
+    fmt.Println(nextInt())
+    // To confirm that the state is unique to that
+    // particular function, create and test a new one.
+    newInts := intSeq()
+    fmt.Println(newInts()) }
